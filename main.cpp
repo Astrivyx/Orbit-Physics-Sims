b3b62c7a3735093c9b48cb51427c475aadbb60c6
Astrivyx <yeetusmcfeetus871@gmail.com>
2026-07-12 00:56:37 -0400

    a lot ig

--- main.cpp
+++ main.cpp
@@ -26,13 +26,7 @@ std::vector<float> PlanetMath(float Radius,float x,float y)
             Verticies.push_back(y+(Radius * sin(angle)));
             Verticies.push_back(0.0f);
         }
-        for (int i = 0; i < Verticies.size(); i += 3)
-        {
-            std::cout << "v" << i/3 << ": ("
-                    << Verticies[i]     << ", "
-                    << Verticies[i + 1] << ", "
-                    << Verticies[i + 2] << ")\n";
-        }
+        glBufferSubData(GL_ARRAY_BUFFER,0,Verticies.size()*sizeof(float),Verticies.data());
         return Verticies;
     };
 class Planet
@@ -40,18 +34,26 @@ class Planet
     public:
 
     double Mass;
-    double Vel;
+    double xVel;
+    double yVel;
     double Xpos;
     double Ypos;
     double Radius;
+    float Red;
+    float Green;
+    float Blue;
 
-    Planet(double M,double R,double A,double X,double Y)
+    Planet(double M,double Rad,double Xv,double Yv,double X,double Y,float R,float G,float B)
     {
         Mass = M;
-        Radius = R;
-        Vel = A;
+        Radius = Rad;
+        xVel = Xv;
+        yVel = Yv;
         Xpos = X;
         Ypos = Y;
+        Red = R;
+        Green = G;
+        Blue = B;
     }
     
     void PosChange(double x,double y)
@@ -61,22 +63,75 @@ class Planet
      PlanetMath(Radius,Xpos,Ypos);
     }
     
+    void VelocityChange(double x,double y)
+    {
+     xVel += x;
+     yVel += y;
+    }
+
+    void Movement(double Dt)
+    {
+        Xpos += xVel*Dt;
+        Ypos += yVel*Dt;
+        PlanetMath(Radius,Xpos,Ypos);
+    }
 
 };
 
+void PlanetRenderCall(Planet& Planet,GLint FragColor,GLuint PlanetVBO,GLuint PlanetVAO,double Dt)
+{
+    glUniform4f(FragColor, Planet.Red, Planet.Green, Planet.Blue, 0.0f);
+    glBindBuffer(GL_ARRAY_BUFFER, PlanetVBO); // bind buffer so it moves the current obj
+    glBindVertexArray(PlanetVAO);
+    Planet.Movement(Dt);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 50+2);
+}
 
     void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     {
     glViewport(0, 0, width, height);
     }  
 
-    void processInput(GLFWwindow *window) // uses window to process input
+    void processInput(GLFWwindow *window,Planet& Earth,Planet& Venus,double Dt) // uses window to process input
     {
         if(glfwGetKey(window,GLFW_KEY_ESCAPE) == GLFW_PRESS) // uses glfw to detect key pressed while user is in window
         {
             glfwSetWindowShouldClose(window, true); // closes
         }
+        if(glfwGetKey(window,GLFW_KEY_D) == GLFW_PRESS) // uses glfw to detect key pressed while user is in window
+        {
+            Earth.VelocityChange(200*Dt,0); // Changes x vel
+        }
+        if(glfwGetKey(window,GLFW_KEY_A) == GLFW_PRESS) // uses glfw to detect key pressed while user is in window
+        {
+            Earth.VelocityChange(-200*Dt,0); // Changes x vel
+        }
+        if(glfwGetKey(window,GLFW_KEY_W) == GLFW_PRESS) // uses glfw to detect key pressed while user is in window
+        {
+            Earth.VelocityChange(0,200*Dt); // Changes y vel
+        }
+        if(glfwGetKey(window,GLFW_KEY_S) == GLFW_PRESS) // uses glfw to detect key pressed while user is in window
+        {
+            Earth.VelocityChange(0,-200*Dt); // Changes y vel
+        }
+        if(glfwGetKey(window,GLFW_KEY_RIGHT) == GLFW_PRESS) // uses glfw to detect key pressed while user is in window
+        {
+            Venus.VelocityChange(200*Dt,0); // Changes x vel
+        }
+        if(glfwGetKey(window,GLFW_KEY_LEFT) == GLFW_PRESS) // uses glfw to detect key pressed while user is in window
+        {
+            Venus.VelocityChange(-200*Dt,0); // Changes x vel
+        }
+        if(glfwGetKey(window,GLFW_KEY_UP) == GLFW_PRESS) // uses glfw to detect key pressed while user is in window
+        {
+            Venus.VelocityChange(0,200*Dt); // Changes y vel
+        }
+        if(glfwGetKey(window,GLFW_KEY_DOWN) == GLFW_PRESS) // uses glfw to detect key pressed while user is in window
+        {
+            Venus.VelocityChange(0,-200*Dt); // Changes y vel
+        }
     }
+
 int main()
 {
     // ---- WINDOW SETUP ----
@@ -104,20 +159,21 @@ int main()
     glfwSetFramebufferSizeCallback(OrbitSim, framebuffer_size_callback);
 
 
-    Planet Earth = Planet(323753.610726,600,0,200,600);
-    Planet Venus = Planet(263840.598674,0,0,0,1);
+    Planet Earth = Planet(323753.610726,600,0,0,200,600,0.0f,1.0f,0.0f);
+    Planet Venus = Planet(263840.598674,550,0,0,400,900,1.0f,0.0f,0.0f);
+
     // ---- VERTEX DATA ----
     std::vector<float> vertices = PlanetMath(Earth.Radius,Earth.Xpos,Earth.Ypos);
-
+    std::vector<float> Venusvert = PlanetMath(Venus.Radius,Venus.Xpos,Venus.Ypos);
     // ---- BUFFER SETUP ----
-    unsigned int VBO;
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    GLuint Planet1VAO , Planet1VBO;
+    glGenVertexArrays(1, &Planet1VAO);
+    glGenBuffers(1, &Planet1VBO);
 
-    glBindVertexArray(VAO); // bind VAO first, then VBO
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindVertexArray(Planet1VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, Planet1VBO);
     glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(float), vertices.data(), GL_DYNAMIC_DRAW);
+    // Configure vertex attribute pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -146,9 +202,10 @@ int main()
     // ---- FRAGMENT SHADER ----
     const char *fragmentShaderSource = "#version 330 core\n"
         "out vec4 FragColor;\n"
+        "uniform vec4 myCustomColor;\n"
         "void main()\n"
         "{\n"
-        "   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+        "   FragColor = myCustomColor;\n"
         "}\0";
 
     unsigned int fragmentShader;
@@ -164,6 +221,20 @@ int main()
     glLinkProgram(shaderProgram);
 
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+    GLuint Planet2VAO , Planet2VBO;
+    glGenVertexArrays(1, &Planet2VAO);
+    glGenBuffers(1, &Planet2VBO);
+
+    glBindVertexArray(Planet2VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, Planet2VBO);
+    glBufferData(GL_ARRAY_BUFFER, Venusvert.size()*sizeof(float), Venusvert.data(), GL_DYNAMIC_DRAW);
+    // Configure vertex attribute pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+// Unbind to secure the state
+glBindVertexArray(0);
     if (!success) {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::Program::COMPILATION_FAILED\n" << infoLog << std::endl;
@@ -172,24 +243,29 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    GLint FragColor = glGetUniformLocation(shaderProgram,"myCustomColor");
     // ---- RENDER LOOP ----
     double LastFrame = 0;
     double xPosChange = 0;
     while (!glfwWindowShouldClose(OrbitSim))
     {
         double StartFrameTime = glfwGetTime();
-        processInput(OrbitSim);
+        processInput(OrbitSim,Earth,Venus,StartFrameTime-LastFrame);
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        
         // draw calls go INSIDE the loop
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 50+2);
+
+        PlanetRenderCall(Earth,FragColor,Planet1VBO,Planet1VAO,StartFrameTime-LastFrame);
+
+
+        PlanetRenderCall(Venus,FragColor,Planet2VBO,Planet2VAO,StartFrameTime-LastFrame);
 
         glfwSwapBuffers(OrbitSim);
         glfwPollEvents();
-        std::cout<<StartFrameTime-LastFrame<<std::endl;
+
         std::cout<<"Fps Per Second:"<<1/(StartFrameTime-LastFrame)<<std::endl;
         LastFrame = StartFrameTime;
     }
